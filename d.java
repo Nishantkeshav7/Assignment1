f36b7283418943627ec5044b52142b146be422c2
nishant keshav <nishantkeshavcr7@gmail.com>
2021-02-27 18:18:06 +0530

    6th Commit

---
+++ d.java
@@ -0,0 +1,8 @@
+public class d {
+    public static void main(String[]args) {
+        String str = "Hello";
+        for (int i = 1 ; i <= 10 ; i++) {
+            System.out.println(str);
+        }
+    }
+}
